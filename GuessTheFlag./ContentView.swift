ff8def0170233a0b4a98fed3a93dadc5aec7aeb1
Sebastien REMY <seb.remy@me.com>
2020-06-13 11:31:53 +0200

    Initial Commit

---
+++ GuessTheFlag./ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag.
+//
+//  Created by Sebastien REMY on 13/06/2020.
+//  Copyright © 2020 MonkeyDev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
