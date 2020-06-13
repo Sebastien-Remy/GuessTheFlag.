6abfd7be10d389b683eb64a7c5eb50da77de6cfd
Sebastien REMY <seb.remy@me.com>
2020-06-13 18:40:38 +0200

    Wrap up

--- GuessTheFlag./ContentView.swift
+++ GuessTheFlag./ContentView.swift
@@ -17,6 +17,12 @@ struct ContentView: View {
     @State private var alertMessage = ""
     @State private var score = 0
     
+    
+    
+    @State private var rotationAmount = 0.0
+    @State private var showOpacity = false
+    @State private var revealCorrect = false
+    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
@@ -33,14 +39,27 @@ struct ContentView: View {
                 ForEach (0..<3) { number in
                     Button(action: {
                         self.flagTapped(number)
-                    }) {
+                        withAnimation {
+                            self.rotationAmount += 360
+                            self.showOpacity = true
+                        }
+                    })
+                    {
                         Image(self.countries[number].lowercased())
                             .renderingMode(.original)
                             .clipShape(Capsule())
                             .overlay(Capsule()
                                 .stroke(Color.black, lineWidth: 1))
                             .shadow(color: .black, radius: 2)
+                            .scaleEffect((number == self.correctAnswer && self.revealCorrect) ? 1.3 : 1)
+                            .animation(.spring())
+                        
                     }
+                        
+                    .rotation3DEffect(.degrees(number == self.correctAnswer ? self.rotationAmount : 0), axis: (x: 0, y: 1, z: 0))
+                    .opacity( (number == self.correctAnswer || self.revealCorrect) ? 1.0 : self.showOpacity ? 0.2 : 1.0 )
+                               
+                        
                 }
                 VStack {
                     Text("Score")
@@ -63,17 +82,30 @@ struct ContentView: View {
     }
     
     func flagTapped(_ number: Int) {
+        
         if number == correctAnswer {
-            score += 1
-            askQuestion()
+            withAnimation {
+                score += 1
+            }
+            alertTitle = "Win"
+            alertMessage = "You score is \(score)!"
         } else {
+            withAnimation {
+                revealCorrect = true
+            }
             alertTitle = "Wrong"
             alertMessage = "You chosse \(countries[number])'s flag."
-            showingAlert = true
         }
+        
+        showingAlert = true
+        
     }
     
     func askQuestion() {
+        withAnimation {
+            self.revealCorrect = false
+            self.showOpacity = false
+        }
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
